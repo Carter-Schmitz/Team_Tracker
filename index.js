05468b364b9900ebfe728e250e24d7314242e839
Carter_Schmitz <carterschmitz95@gmail.com>
2022-12-17 21:16:50 -0600

    restructured index, wrote Engineer constructor

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const fs = require('fs')
 const inquirer = require('inquirer')
 const generateHtml = require('./generateHTML')
 
+// Object constructors for roles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+
 // Questions to be used in inquirer, to gather user input
 function init(){
     inquirer.prompt([
