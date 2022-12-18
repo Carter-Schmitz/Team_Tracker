bebb9cc0590763d04637e8905b93e9d5b3b0bbf1
Carter_Schmitz <carterschmitz95@gmail.com>
2022-12-17 21:09:34 -0600

    Created first test for employee object creation, as well as directory to stor object constructors

---
+++ test/Employee.test.js
@@ -0,0 +1,9 @@
+const Classes = require('../classes');
+
+describe("Classes", () => {
+    describe("employee", () => {
+        it("should take basic infor for all employees", () => {
+            const name = "John Darksouls"
+        })
+    })
+})
