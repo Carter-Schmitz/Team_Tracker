26a9452665e955f2ba98c37bbc6b044c2dc2aa4d
Carter_Schmitz <carterschmitz95@gmail.com>
2022-12-18 20:25:08 -0600

    created gitignore

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -3,21 +3,21 @@ const Engineer = require('../lib/Engineer');
 
 // creating engineer object  
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Carter', 015, 'carterschmitz95@gmail.com', 'carter-schmitz');
+    const engineer = new Engineer('Carter', 15, 'carterschmitz95@gmail.com', 'carter-schmitz');
     
     expect(engineer.github) .toEqual(expect.any(String));
 });
 
 // gets github from getGithub()
 test('gets engineer github value', () => {
-    const engineer = new Engineer('Carter', 015, 'carterschmitz95@gmail.com', 'carter-schmitz');
+    const engineer = new Engineer('Carter', 15, 'carterschmitz95@gmail.com', 'carter-schmitz');
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
 
 // gets role from getRole() 
 test('gets role of employee', () => {
-    const engineer = new Engineer('Carter', 015, 'carterschmitz95@gmail.com', 'carter-schmitz');
+    const engineer = new Engineer('Carter', 15, 'carterschmitz95@gmail.com', 'carter-schmitz');
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
